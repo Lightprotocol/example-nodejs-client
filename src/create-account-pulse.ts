cc9feb796f585933a8832d09275441e5ca5db369
swen <swen-code@swens-MacBook-Air-2.local>
2024-09-13 21:31:28 +0700

    add compressed account pulse scripts

--- src/create-account-pulse.ts
+++ src/create-account-pulse.ts
@@ -1,30 +1,30 @@
 import { 
   compress,
+  createAccount,
   createRpc,
+  LightSystemProgram,
   Rpc,
   transfer,
 } from "@lightprotocol/stateless.js";
 import { SystemProgram, Transaction } from "@solana/web3.js";
 import { PAYER_KEYPAIR, RPC_ENDPOINT } from "./constants";
 import { PublicKey } from "@solana/web3.js";
+import { randomBytes } from "crypto";
 
 const fromKeypair = PAYER_KEYPAIR;
 
 const connection : Rpc= createRpc(RPC_ENDPOINT, RPC_ENDPOINT);
 
-async function runTransferPulse() {
+async function runCreateAccountPulse() {
   try {
     // // Run this once to have compressed lamports!
     // Compress lamports to self
     // const txId = await compress(connection, fromKeypair, 10, fromKeypair.publicKey);
     // console.log("compressed signature:", txId);
-    
     while (true) {
-      // Transfer 10 lamports to self
-      const txId = await transfer(connection, fromKeypair, 10, fromKeypair, fromKeypair.publicKey, undefined, {
-        skipPreflight: false,
-      });
-      console.log(`Compressed SOL Transfer Success. Transaction Signature:`, txId);
+      // Creat account with random address
+      const txId = await createAccount(connection, fromKeypair, randomBytes(32), LightSystemProgram.programId);
+      console.log(`Compressed Account Creation Success. Transaction Signature:`, txId);
 
       // Wait 5 seconds
       await new Promise(resolve => setTimeout(resolve, 5000));
@@ -34,5 +34,5 @@ async function runTransferPulse() {
   }
 }
 
-runTransferPulse();
+runCreateAccountPulse();
 
