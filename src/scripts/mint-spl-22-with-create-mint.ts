14761311a2538f4c10a2abbf9365bd2545337689
Swenschaeferjohann <swen-code@swens-MacBook-Air-2.local>
2024-12-16 18:29:52 +0000

    add example for versionedTx with t22 mints + CompressedTokenProgram.create-mint

---
+++ src/scripts/mint-spl-22-with-create-mint.ts
@@ -0,0 +1,154 @@
+import { Rpc, createRpc, sendAndConfirmTx } from "@lightprotocol/stateless.js";
+import {
+  compress,
+  CompressedTokenProgram,
+  transfer,
+} from "@lightprotocol/compressed-token";
+import {
+  getOrCreateAssociatedTokenAccount,
+  mintTo as mintToSpl,
+  TOKEN_2022_PROGRAM_ID,
+  createInitializeMetadataPointerInstruction,
+  ExtensionType,
+  getMintLen,
+  LENGTH_SIZE,
+  TYPE_SIZE,
+} from "@solana/spl-token";
+import { PAYER_KEYPAIR, RPC_ENDPOINT } from "../constants";
+import {
+  Keypair,
+  TransactionMessage,
+  VersionedTransaction,
+} from "@solana/web3.js";
+import {
+  createInitializeInstruction,
+  pack,
+  TokenMetadata,
+} from "@solana/spl-token-metadata";
+
+const payer = PAYER_KEYPAIR;
+const connection: Rpc = createRpc(RPC_ENDPOINT, RPC_ENDPOINT);
+
+(async () => {
+  const mint = Keypair.generate();
+  const decimals = 9;
+
+  const metadata: TokenMetadata = {
+    mint: mint.publicKey,
+    name: "TEST_CTOKEN_22",
+    symbol: "CTOK22",
+    uri: "https://genius.com/Rick-astley-never-gonna-give-you-up-lyrics",
+    additionalMetadata: [["rick", "rolled"]],
+  };
+
+  const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+
+  const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length;
+
+  // airdrop for gas
+  // await confirmTx(
+  //   connection,
+  //   await connection.requestAirdrop(payer.publicKey, 1e7)
+  // );
+
+  const mintLamports = await connection.getMinimumBalanceForRentExemption(
+    mintLen + metadataLen
+  );
+
+  const [createMintAccountIx, initializeMintIx, createTokenPoolIx] =
+    await CompressedTokenProgram.createMint({
+      feePayer: payer.publicKey,
+      authority: payer.publicKey,
+      mint: mint.publicKey,
+      decimals,
+      freezeAuthority: null,
+      rentExemptBalance: mintLamports,
+      tokenProgramId: TOKEN_2022_PROGRAM_ID,
+      mintSize: mintLen,
+    });
+
+  const instructions = [
+    createMintAccountIx,
+    createInitializeMetadataPointerInstruction(
+      mint.publicKey,
+      payer.publicKey,
+      mint.publicKey,
+      TOKEN_2022_PROGRAM_ID
+    ),
+    initializeMintIx,
+    createInitializeInstruction({
+      programId: TOKEN_2022_PROGRAM_ID,
+      mint: mint.publicKey,
+      metadata: mint.publicKey,
+      name: metadata.name,
+      symbol: metadata.symbol,
+      uri: metadata.uri,
+      mintAuthority: payer.publicKey,
+      updateAuthority: payer.publicKey,
+    }),
+    createTokenPoolIx,
+  ];
+
+  const messageV0 = new TransactionMessage({
+    payerKey: payer.publicKey,
+    recentBlockhash: (await connection.getLatestBlockhash()).blockhash,
+    instructions,
+  }).compileToV0Message();
+
+  const mintTransaction = new VersionedTransaction(messageV0);
+  mintTransaction.sign([payer, mint]);
+
+  const txId = await sendAndConfirmTx(connection, mintTransaction);
+
+  console.log(`txId: ${txId}`);
+  const ata = await getOrCreateAssociatedTokenAccount(
+    connection,
+    payer,
+    mint.publicKey,
+    payer.publicKey,
+    undefined,
+    undefined,
+    undefined,
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  console.log(`ATA: ${ata.address}`);
+  /// Mint SPL
+  const mintTxId = await mintToSpl(
+    connection,
+    payer,
+    mint.publicKey,
+    ata.address,
+    payer.publicKey,
+    1e5,
+    undefined,
+    undefined,
+    TOKEN_2022_PROGRAM_ID
+  );
+  console.log(`mint-spl success! txId: ${mintTxId}`);
+
+  const compressedTokenTxId = await compress(
+    connection,
+    payer,
+    mint.publicKey,
+    1e5,
+    payer,
+    ata.address,
+    payer.publicKey,
+    undefined,
+    undefined,
+    TOKEN_2022_PROGRAM_ID
+  );
+  console.log(`compressed-token success! txId: ${compressedTokenTxId}`);
+
+  // compressed-transfers do not require passing of token program id.
+  const transferCompressedTxId = await transfer(
+    connection,
+    payer,
+    mint.publicKey,
+    1e5,
+    payer,
+    payer.publicKey // self-transfer
+  );
+  console.log(`transfer-compressed success! txId: ${transferCompressedTxId}`);
+})();
