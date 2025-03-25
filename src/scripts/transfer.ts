37e989669b8328f9c46823eafb526ca388cef78e
Swenschaeferjohann <swen@lightprotocol.com>
2025-03-25 01:07:40 +0000

    clean transfer.ts

--- src/scripts/transfer.ts
+++ src/scripts/transfer.ts
@@ -8,13 +8,9 @@ import {
 } from "@lightprotocol/stateless.js";
 import {
   CompressedTokenProgram,
-  createMint,
-  mintTo,
   selectMinCompressedTokenAccountsForTransfer,
-  selectMinCompressedTokenAccountsForTransferOrPartial,
-  transfer,
 } from "@lightprotocol/compressed-token";
-import { ComputeBudgetProgram, Keypair } from "@solana/web3.js";
+import { ComputeBudgetProgram } from "@solana/web3.js";
 import {
   PAYER_KEYPAIR,
   RPC_ENDPOINT,
@@ -31,20 +27,9 @@ console.log(tokenRecipient.publicKey.toBase58());
 
 /// Uncomment to use env:
 const connection: Rpc = createRpc(RPC_ENDPOINT, RPC_ENDPOINT, RPC_ENDPOINT);
+const amount = new BN(100);
 
 (async () => {
-  /// Transfer compressed tokens
-  // const transferTxId = await transfer(
-  //   connection,
-  //   payer,
-  //   MINT_ADDRESS,
-  //   555,
-  //   payer,
-  //   tokenRecipient.publicKey
-  // );
-
-  // console.log(`transfer success! txId: ${transferTxId}`);
-
   const compressedTokenAccounts =
     await connection.getCompressedTokenAccountsByOwner(payer.publicKey, {
       mint: MINT_ADDRESS,
@@ -56,17 +41,11 @@ const connection: Rpc = createRpc(RPC_ENDPOINT, RPC_ENDPOINT, RPC_ENDPOINT);
       .toString()
   );
 
-  const [inputAccounts] = selectMinCompressedTokenAccountsForTransferOrPartial(
+  const [inputAccounts] = selectMinCompressedTokenAccountsForTransfer(
     compressedTokenAccounts.items,
-    new BN(555)
-  );
-  console.log(
-    inputAccounts
-      .reduce((acc, curr) => acc.add(curr.parsed.amount), new BN(0))
-      .toString()
+    amount
   );
 
-  return;
   const proof = await connection.getValidityProof(
     inputAccounts.map((account) => bn(account.compressedAccount.hash))
   );
@@ -75,7 +54,7 @@ const connection: Rpc = createRpc(RPC_ENDPOINT, RPC_ENDPOINT, RPC_ENDPOINT);
     payer: payer.publicKey,
     inputCompressedTokenAccounts: inputAccounts,
     toAddress: tokenRecipient.publicKey,
-    amount: new BN(555),
+    amount,
     recentInputStateRootIndices: proof.rootIndices,
     recentValidityProof: proof.compressedProof,
     outputStateTrees: undefined,
@@ -83,8 +62,9 @@ const connection: Rpc = createRpc(RPC_ENDPOINT, RPC_ENDPOINT, RPC_ENDPOINT);
 
   const { blockhash } = await connection.getLatestBlockhash();
   const additionalSigners = dedupeSigner(payer, [payer]);
+
   const signedTx = buildAndSignTx(
-    [ComputeBudgetProgram.setComputeUnitLimit({ units: 1_000_000 }), ix],
+    [ComputeBudgetProgram.setComputeUnitLimit({ units: 350_000 }), ix],
     payer,
     blockhash,
     additionalSigners
