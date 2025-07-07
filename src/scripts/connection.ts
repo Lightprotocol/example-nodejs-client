44947367181eb61b9ce881a456c4d9668db12b17
Swenschaeferjohann <swen@lightprotocol.com>
2025-07-07 13:04:31 -0400

    connection

--- src/scripts/connection.ts
+++ src/scripts/connection.ts
@@ -1,6 +1,6 @@
 import { Rpc } from "@lightprotocol/stateless.js";
-import { MINT_ADDRESS, RPC_ENDPOINT } from "../constants";
-import { PublicKey } from "@solana/web3.js";
+import { RPC_ENDPOINT, PAYER_KEYPAIR } from "../constants";
+
 const stateless = require("@lightprotocol/stateless.js");
 
 const connection: Rpc = stateless.createRpc(RPC_ENDPOINT, RPC_ENDPOINT);
@@ -12,16 +12,9 @@ const connection: Rpc = stateless.createRpc(RPC_ENDPOINT, RPC_ENDPOINT);
   const health = await connection.getIndexerHealth();
   console.log("Indexer Health:", health);
 
-  // const accs = await connection.getCompressedAccountsByOwner(
-  //   new PublicKey("6MZszp7ihPjUeoi8RJs9NNC4jBxi7beiqvXHJhxd7fe")
-  // );
   console.log("connection", connection.rpcEndpoint);
   const accs = await connection.getCompressedTokenAccountsByOwner(
-    new PublicKey("6MZszp7ihPjUeoi8RJs9NNC4jBxi7beiqvXHJhxd7fe"),
-    {
-      mint: MINT_ADDRESS,
-      // mint: new PublicKey("APMHh8w6ZWLCv4M5daKAvGzcFLemFPvrNfdHiTLEYghK"),
-    }
+    PAYER_KEYPAIR.publicKey
   );
-  console.log(accs);
+  console.log("compressed token accounts: ", accs);
 })();
